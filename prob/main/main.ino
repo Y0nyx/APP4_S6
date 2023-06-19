d7b6037cc7b22965a7439f0ff556f9cb6b317230
Y0nyx <72567319+Y0nyx@users.noreply.github.com>
2023-06-19 14:56:55 -0400

    change code

--- prob/main/main.ino
+++ prob/main/main.ino
@@ -1,100 +1,65 @@
-#include <Manchester.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
-#define TX_PIN 1
-#define RX_PIN 3
-
-void TaskReceiveManchester(void *pvParameters);
-void TaskSendManchester(void *pvParameters);
-
-TaskHandle_t receive_manchester_handle;
-TaskHandle_t send_manchester_handle;
-
+#define TX_PIN 13
+#define RX_PIN 12
+#define BIT_PERIOD 2000  // Bit period in microseconds
 
 void setup() {
-  Serial.begin(9600);
-  man.workAround1MhzTinyCore();
-  man.setupTransmit(TX_PIN, MAN_9600);
-
-  xTaskCreate(
-    TaskReceiveManchester,
-    "Receive Manchester",
-    2048,
-    NULL,
-    1,
-    receive_manchester_handle
-  );
-
-  xTaskCreate(
-    TaskSendManchester,
-    "Send Manchester",
-    2048, 
-    NULL,
-    2,
-    send_manchester_handle
-  )
+  pinMode(TX_PIN, OUTPUT);
+  pinMode(RX_PIN, INPUT);
+  Serial.begin(115200);
+  
+  // Create the Manchester receiver task
+  xTaskCreate(receiveManchesterTask, "ManchRx", 1024, NULL, 1, NULL);
+  
+  // Create the Manchester transmitter task
+  xTaskCreate(sendManchesterTask, "ManchTx", 1024, NULL, 1, NULL);
 }
 
-void loop(){
-  if(receive_manchester_handle != NULL){ 
-    delay(10000);
-    vTaskDelete(receive_manchester_handle); 
-    receive_manchester_handle = NULL; 
-  }
+void loop() {
+  // Nothing needed here, all work is done in the tasks
+}
 
-  if(send_manchester_handle != NULL){ 
-    delay(10000);
-    vTaskDelete(send_manchester_handle); 
-    send_manchester_handle = NULL; 
+void receiveManchesterTask(void *parameter) {
+  while (1) {
+    byte data = 0;
+    for (int i = 7; i >= 0; i--) {
+      // Wait for a transition
+      while (digitalRead(RX_PIN) == HIGH);
+      while (digitalRead(RX_PIN) == LOW);
+      
+      // Wait for half a bit period and then read the bit
+      vTaskDelay(BIT_PERIOD / 2 / portTICK_PERIOD_MS);
+      bool bit = digitalRead(RX_PIN);
+      data |= (bit << i);
+    }
+    
+    // Print out the received byte
+    Serial.println(data, HEX);
   }
 }
 
-
-
-/*--------------------------------------------------*/
-/*---------------------- Tasks ---------------------*/
-/*--------------------------------------------------*/
-
-void TaskBlink(void *pvParameters){  // This is a task.
-  uint32_t blink_delay = *((uint32_t*)pvParameters);
-
-  // initialize digital LED_BUILTIN on pin 13 as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  for (;;){ // A Task shall never return or exit.
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    // arduino-esp32 has FreeRTOS configured to have a tick-rate of 1000Hz and portTICK_PERIOD_MS
-    // refers to how many milliseconds the period between each ticks is, ie. 1ms.
-    delay(blink_delay);
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    delay(blink_delay);
+void sendManchesterTask(void *parameter) {
+  byte data = 0x5A;  // Example data byte to send
+  
+  while (1) {
+    for (int i = 7; i >= 0; i--) {
+      bool bit = data & (1 << i);
+      if (bit) {
+        digitalWrite(TX_PIN, HIGH);
+        vTaskDelay(BIT_PERIOD / 2 / portTICK_PERIOD_MS);
+        digitalWrite(TX_PIN, LOW);
+        vTaskDelay(BIT_PERIOD / 2 / portTICK_PERIOD_MS);
+      } else {
+        digitalWrite(TX_PIN, LOW);
+        vTaskDelay(BIT_PERIOD / 2 / portTICK_PERIOD_MS);
+        digitalWrite(TX_PIN, HIGH);
+        vTaskDelay(BIT_PERIOD / 2 / portTICK_PERIOD_MS);
+      }
+    }
+    
+    // Delay before sending the next byte
+    vTaskDelay(BIT_PERIOD / portTICK_PERIOD_MS);
   }
-}
-
-void TaskAnalogRead(void *pvParameters){  // This is a task.
-  (void) pvParameters;
-  // Check if the given analog pin is usable - if not - delete this task
-  if(!adcAttachPin(ANALOG_INPUT_PIN)){
-    Serial.printf("TaskAnalogRead cannot work because the given pin %d cannot be used for ADC - the task will delete itself.\n", ANALOG_INPUT_PIN);
-    analog_read_task_handle = NULL; // Prevent calling vTaskDelete on non-existing task
-    vTaskDelete(NULL); // Delete this task
-  }
-
-  for (;;){
-    // read the input on analog pin:
-    int sensorValue = analogRead(ANALOG_INPUT_PIN);
-    // print out the value you read:
-    Serial.println(sensorValue);
-    delay(100); // 100ms delay
-  }
-}
-
-void TaskReceiveManchester(void *pvParameters){
-
-}
-
-void TaskSendManchester(void *pvParameters){
-  (void) pvParameters;
-
-
-
 }
